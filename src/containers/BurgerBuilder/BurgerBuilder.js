6b1634d18cfde096dbf143fcd8c2aeb766905f7a
likhith chinnam <31769925+likhith10@users.noreply.github.com>
2018-03-28 18:08:03 -0400

    Add files via upload
    Added HTTP Post  and data is stored in firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -83,7 +84,22 @@ purchaseCancelHandler = () => {
   this.setState({purchasing: false});  
 }
 purchaseContinueHandler = () => {
-    alert('You Continue!');
+    //alert('You Continue!');
+    const order = {
+        ingredients: this.state.ingredients,
+        price: this.state.totalPrice,
+        customer: {
+            name: 'Likhith',
+            adress: {
+                street: '9501',
+                zipCode: '28262',
+                country: 'United States'
+            },
+            email:'chinnamlikhith@gmail.com',
+        },
+        deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order).then(response => console.log(response)).catch(error => console.log(error));
 } 
     render() {
         const disabledInfo = {
