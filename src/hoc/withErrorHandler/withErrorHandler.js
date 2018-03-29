7cdee6d5c750b15113db15d59b0beaae01a26a53
likhith chinnam <31769925+likhith10@users.noreply.github.com>
2018-03-29 12:23:21 -0400

    Add files via upload
    Added ComponentWillUnmount() to remove unwanted Old Interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,15 +10,19 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+          this.reqInterceptor =  axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
         }
+componentWillUnmount() {
+    axios.interceptors.eject(this.reqInterceptor);
+    axios.interceptors.eject(this.resInterceptor);
 
+}
         errorConfirmedHandler = () => {
             this.setState({error: null});
         }
