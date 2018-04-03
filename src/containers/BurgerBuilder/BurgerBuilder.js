80114e240f6dcfded96c8a0f888d1bd4be4c0052
likhith chinnam <31769925+likhith10@users.noreply.github.com>
2018-04-03 15:09:40 -0400

    Add files via upload
    Added queryParams for getting Total Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,17 @@ purchaseCancelHandler = () => {
   this.setState({purchasing: false});  
 }
 purchaseContinueHandler = () => {
-    //alert('You Continue!');
-    this.setState({loading: true});
-    const order = {
-        ingredients: this.state.ingredients,
-        price: this.state.totalPrice,
-        customer: {
-            name: 'Likhith',
-            adress: {
-                street: '9501',
-                zipCode: '28262',
-                country: 'United States'
-            },
-            email:'chinnamlikhith@gmail.com',
-        },
-        deliveryMethod: 'fastest'
+   
+    const queryParams = [];
+    queryParams.push('price=' + this.state.totalPrice);
+   
+    for (let i in this.state.ingredients) {
+        queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-    .then(response => {
-        this.setState({loading: false, purchasing: false});
-    }).catch(error => {
-        this.setState({loading: false,  purchasing: false});
-    })        
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+        pathname: '/checkout',
+    search:'?'+ queryString});      
 } 
     render() {
         const disabledInfo = {
