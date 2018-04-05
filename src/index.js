710e18cc92450fe38371e7117ba0e004f4171852
likhith chinnam <31769925+likhith10@users.noreply.github.com>
2018-04-05 16:36:36 -0400

    Add files via upload
    Using thunk, ApplyMiddleware for implementing Advanced Redux Feature :  Use of  Asynchronous code inside ActionCreators

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'; 
-import {createStore} from 'redux';
-import reducer from './Store/reducer';
+import {createStore, applyMiddleware, compose} from 'redux';
+import burgerBuilderReducer from './Store/reducers/burgerBuilder';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer);
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 const app = (
     <Provider store = {store}>
     <BrowserRouter>
